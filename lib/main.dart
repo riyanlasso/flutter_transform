140ac8d5cb1a0817afebe48e8db7001e3ab70488
riyanlasso <riyanlasso@gmail.com>
2022-09-19 14:02:04 +0700

    Perspective on Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
+  Offset _offset = Offset(0.4, 0.7);
   void _incrementCounter() {
     setState(() {
       _counter++;
@@ -39,96 +39,136 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: Text(widget.title),
+  //     ),
+  //     body: Center(
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: <Widget>[
+  //           const Text(
+  //             'You have pushed the button this many times:',
+  //           ),
+  //           Text(
+  //             '$_counter',
+  //             style: Theme.of(context).textTheme.headline4,
+  //           ),
+  //           Transform.rotate(
+  //             angle: -45 * (pi / 180.0),
+  //             child: ElevatedButton(
+  //               child: const Text("Rotated button"),
+  //               onPressed: () {},
+  //             ),
+  //           ),
+  //           Transform(
+  //             transform: Matrix4.rotationZ(-45 * (pi / 180.0)),
+  //             alignment: Alignment.center,
+  //             child: ElevatedButton(
+  //               child: const Text("Rotated button"),
+  //               onPressed: () {},
+  //             ),
+  //           ),
+  //           Transform.scale(
+  //             scale: 2.0,
+  //             child: ElevatedButton(
+  //               child: const Text("scaled up"),
+  //               onPressed: () {},
+  //             ),
+  //           ),
+  //           Transform(
+  //             transform: Matrix4.identity()..scale(2.0, 2.0),
+  //             alignment: Alignment.center,
+  //             child: ElevatedButton(
+  //               child: const Text("scaled up (matrix)"),
+  //               onPressed: () {},
+  //             ),
+  //           ),
+  //           Transform.translate(
+  //             offset: const Offset(100, 300),
+  //             child: ElevatedButton(
+  //               child: const Text("translated to bottom"),
+  //               onPressed: () {},
+  //             ),
+  //           ),
+  //           Transform(
+  //             transform: Matrix4.translationValues(100, 300, 0),
+  //             child: ElevatedButton(
+  //               child: const Text("translated to bottom (matrix)"),
+  //               onPressed: () {},
+  //             ),
+  //           ),
+  //           Transform.translate(
+  //             offset: const Offset(70, 200),
+  //             child: Transform.rotate(
+  //               angle: -45 * (pi / 180.0),
+  //               child: Transform.scale(
+  //                 scale: 2.0,
+  //                 child: ElevatedButton(
+  //                   child: const Text("multiple transformations"),
+  //                   onPressed: () {},
+  //                 ),
+  //               ),
+  //             ),
+  //           ),
+  //           Transform(
+  //             alignment: Alignment.center,
+  //             transform: Matrix4.translationValues(70, 200, 0)
+  //               ..rotateZ(-45 * (pi / 180.0))
+  //               ..scale(2.0, 2.0),
+  //             child: ElevatedButton(
+  //               child: const Text("multiple transformations (matrix)"),
+  //               onPressed: () {},
+  //             ),
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //     floatingActionButton: FloatingActionButton(
+  //       onPressed: _incrementCounter,
+  //       tooltip: 'Increment',
+  //       child: const Icon(Icons.add),
+  //     ),
+  //   );
+  // }
   Widget build(BuildContext context) {
+    return Transform(
+        transform: Matrix4.identity()
+          ..setEntry(3, 2, 0.001)
+          ..rotateX(0.01 * _offset.dy)
+          ..rotateY(-0.01 * _offset.dx),
+        alignment: FractionalOffset.center,
+        child: GestureDetector(
+          onPanUpdate: (details) => setState(() => _offset += details.delta),
+          onDoubleTap: () => setState(() => _offset = Offset.zero),
+          child: _defaultApp(context),
+        ));
+  }
+
+  _defaultApp(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text('The Matrix 3D'),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
+          children: [
+            Text(
               'You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
-            Transform.rotate(
-              angle: -45 * (pi / 180.0),
-              child: ElevatedButton(
-                child: const Text("Rotated button"),
-                onPressed: () {},
-              ),
-            ),
-            Transform(
-              transform: Matrix4.rotationZ(-45 * (pi / 180.0)),
-              alignment: Alignment.center,
-              child: ElevatedButton(
-                child: const Text("Rotated button"),
-                onPressed: () {},
-              ),
-            ),
-            Transform.scale(
-              scale: 2.0,
-              child: ElevatedButton(
-                child: const Text("scaled up"),
-                onPressed: () {},
-              ),
-            ),
-            Transform(
-              transform: Matrix4.identity()..scale(2.0, 2.0),
-              alignment: Alignment.center,
-              child: ElevatedButton(
-                child: const Text("scaled up (matrix)"),
-                onPressed: () {},
-              ),
-            ),
-            Transform.translate(
-              offset: const Offset(100, 300),
-              child: ElevatedButton(
-                child: const Text("translated to bottom"),
-                onPressed: () {},
-              ),
-            ),
-            Transform(
-              transform: Matrix4.translationValues(100, 300, 0),
-              child: ElevatedButton(
-                child: const Text("translated to bottom (matrix)"),
-                onPressed: () {},
-              ),
-            ),
-            Transform.translate(
-              offset: const Offset(70, 200),
-              child: Transform.rotate(
-                angle: -45 * (pi / 180.0),
-                child: Transform.scale(
-                  scale: 2.0,
-                  child: ElevatedButton(
-                    child: const Text("multiple transformations"),
-                    onPressed: () {},
-                  ),
-                ),
-              ),
-            ),
-            Transform(
-              alignment: Alignment.center,
-              transform: Matrix4.translationValues(70, 200, 0)
-                ..rotateZ(-45 * (pi / 180.0))
-                ..scale(2.0, 2.0),
-              child: ElevatedButton(
-                child: const Text("multiple transformations (matrix)"),
-                onPressed: () {},
-              ),
-            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: Icon(Icons.add),
       ),
     );
   }
