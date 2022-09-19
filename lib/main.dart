767e70481d1b74286652a6ce7774f6f66ee60145
riyanlasso <riyanlasso@gmail.com>
2022-09-19 18:41:35 +0700

    3D flip animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,122 @@
+import 'package:flip_panel/flip_panel.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
 import 'dart:math';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData.light().copyWith(
+        scaffoldBackgroundColor: Colors.white,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'FlipPanel',
+      home: FlipClockPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
+class FlipClockPage extends StatelessWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FlipClock'),
+      ),
+      body: Center(
+        child: SizedBox(
+          height: 64.0,
+          child: FlipClock.simple(
+            startTime: DateTime.now(),
+            digitColor: Colors.white,
+            backgroundColor: Colors.black,
+            digitSize: 48.0,
+            borderRadius: const BorderRadius.all(Radius.circular(3.0)),
+          ),
+        ),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  Offset _offset = Offset(0.4, 0.7);
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
-  @override
+// import 'package:flutter/material.dart';
+// import 'dart:math';
+
+// void main() {
+//   runApp(FlipWidget());
+// }
+
+// class FlipWidget extends StatelessWidget {
+//   Widget? child;
+
+//   FlipWidget({Key? key, this.child}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Column(
+//       mainAxisSize: MainAxisSize.min,
+//       children: [
+//         Transform(
+//           transform: Matrix4.rotationX(pi / 4),
+//           alignment: Alignment.bottomCenter,
+//           child: ClipRect(
+//               child: Align(
+//             alignment: Alignment.topCenter,
+//             heightFactor: 0.5,
+//             child: child,
+//           )),
+//         ),
+//         Padding(
+//           padding: EdgeInsets.only(top: 2.0),
+//         ),
+//         ClipRect(
+//             child: Align(
+//           alignment: Alignment.bottomCenter,
+//           heightFactor: 0.5,
+//           child: child,
+//         )),
+//       ],
+//     );
+//   }
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
+//     );
+//   }
+// }
+
+// class MyHomePage extends StatefulWidget {
+//   const MyHomePage({super.key, required this.title});
+
+//   final String title;
+
+//   @override
+//   State<MyHomePage> createState() => _MyHomePageState();
+// }
+
+// class _MyHomePageState extends State<MyHomePage> {
+//   int _counter = 0;
+//   Offset _offset = Offset(0.4, 0.7);
+//   void _incrementCounter() {
+//     setState(() {
+//       _counter++;
+//     });
+//   }
+
+  // @override
   // Widget build(BuildContext context) {
   //   return Scaffold(
   //     appBar: AppBar(
@@ -132,44 +210,45 @@ class _MyHomePageState extends State<MyHomePage> {
   //     ),
   //   );
   // }
-  Widget build(BuildContext context) {
-    return Transform(
-        transform: Matrix4.identity()
-          ..setEntry(3, 2, 0.001)
-          ..rotateX(0.01 * _offset.dy)
-          ..rotateY(-0.01 * _offset.dx),
-        alignment: FractionalOffset.center,
-        child: GestureDetector(
-          onPanUpdate: (details) => setState(() => _offset += details.delta),
-          onDoubleTap: () => setState(() => _offset = Offset.zero),
-          child: _defaultApp(context),
-        ));
-  }
+  
+  // Widget build(BuildContext context) {
+  //   return Transform(
+  //       transform: Matrix4.identity()
+  //         ..setEntry(3, 2, 0.001)
+  //         ..rotateX(0.01 * _offset.dy)
+  //         ..rotateY(-0.01 * _offset.dx),
+  //       alignment: FractionalOffset.center,
+  //       child: GestureDetector(
+  //         onPanUpdate: (details) => setState(() => _offset += details.delta),
+  //         onDoubleTap: () => setState(() => _offset = Offset.zero),
+  //         child: _defaultApp(context),
+  //       ));
+  // }
 
-  _defaultApp(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('The Matrix 3D'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
+  // _defaultApp(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       title: Text('The Matrix 3D'),
+  //     ),
+  //     body: Center(
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: [
+  //           Text(
+  //             'You have pushed the button this many times:',
+  //           ),
+  //           Text(
+  //             '$_counter',
+  //             style: Theme.of(context).textTheme.headline4,
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //     floatingActionButton: FloatingActionButton(
+  //       onPressed: _incrementCounter,
+  //       tooltip: 'Increment',
+  //       child: Icon(Icons.add),
+  //     ),
+  //   );
+  // }
+// }
